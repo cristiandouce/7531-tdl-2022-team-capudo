fa9e4fd83dbdab3ef1530ce235e999acdb6fb83f
Cristian Douce <cristiandouce@gmail.com>
2022-07-02 23:55:40 +0000

    Elimino comentarios olvidados

--- proyecto/database/database.go
+++ proyecto/database/database.go
@@ -34,31 +34,3 @@ func GetBicicleteros() (bicicleteros *[]model.Bicicletero, e error) {
 func GetUsuarios() (usuarios *[]model.Usuario, e error) {
 	return parser.GetUsuarios()
 }
-
-// func SearchUsuarios(q QueryUsuario) []model.Usuario {
-// 	var filtered []model.Usuario
-// 	usuarios, err := GetUsuarios()
-
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	for _, usuario := range *usuarios {
-// 		resultAlta :=
-// 	}
-
-// 	return filtered
-// }
-
-// type filterFunc func(model.Usuario) bool
-
-// type QueryUsuario struct {
-// 	FromEdad uint16 `default: 1`
-// 	ToEdad   uint16
-// 	FromAlta time.Time
-// 	ToAlta time.Time
-// }
-
-// func busquedaPorAlta (toAlta time.Time, fromAlta time.Time) {
-
-// }
